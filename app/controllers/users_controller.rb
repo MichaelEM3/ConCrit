cb3e92ecf4a6f1a1ad8001af5d3094faad1b8869
Michael <michaelem3@gmail.com>
2015-07-16 19:04:02 -0400

    Login session works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,26 @@
 class UsersController < ApplicationController
-  before_action :set_user
 
   def new
     @user = User.new
+    if current_user
+      redirect_to '/things'
+    end 
   end
+
 
   def create
     @user = User.new(user_params)
-
     if @user.save
-      redirect_to root_path
+      session[:user_id] = @user.id
+      redirect_to '/things'
     else
-      redirect_to register_path
+      redirect_to '/signup'
     end
-  end
-  
-  def show
   end
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
-
   def user_params
-    params.require(:user).permit(:username, :password, :bio)
+    params.require(:user).permit(:username, :password)
   end
-
 end
