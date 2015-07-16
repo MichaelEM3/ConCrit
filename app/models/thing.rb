d650f1d75ca35e2c9a7606475a63f9fdedae6e5e
Michael <michaelem3@gmail.com>
2015-07-15 22:31:41 -0400

    Added comments

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,4 @@
 class Thing < ActiveRecord::Base
+  validates :description, :name, presence: true
+  has_many :comments
 end
---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :comment, :things_id, :created_at, :updated_at
