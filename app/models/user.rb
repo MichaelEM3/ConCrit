f45192bc1aa71a4da2eb94335dfae7968a790672
Michael <michaelem3@gmail.com>
2015-07-16 20:42:46 -0400

    fixed delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_many :comments 
+  has_many :comments
+
+  validates :username, uniqueness: true
+
+  def self.authenticate(username, password)
+    find_by(username: username).try(:authenticate, password)
+  end
+
 end
